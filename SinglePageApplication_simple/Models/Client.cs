26143fe4e59d7b197a6489c78f24bcbd22148545
FRAJ Saber <frejsaber@gmail.com>
2016-10-29 16:03:55 +0200

    add model

---
+++ SinglePageApplication_simple/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglePageApplication_simple.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public String Login { get; set; }
+        public String Password { get; set; }
+        public DateTime InscriptionDate { get; set; }
+
+    }
+}
