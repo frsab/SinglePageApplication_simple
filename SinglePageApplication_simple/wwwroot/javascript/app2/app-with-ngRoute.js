8ddb7d3dcd2dd9b30be503415ede9e3546d13923
FRAJ Saber <frejsaber@gmail.com>
2017-01-01 14:34:48 +0100

    ajouter les dependances à mon projet

--- SinglePageApplication_simple/wwwroot/javascript/app2/app-with-ngRoute.js
+++ SinglePageApplication_simple/wwwroot/javascript/app2/app-with-ngRoute.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('appWithNgRoute', ['ngRoute']);
+﻿var app = angular.module('appWithNgRoute', ['ngRoute','LocalStorageModule']);
 /***********************************************/
 
 app.config(function ($routeProvider) {
